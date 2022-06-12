8e7fb049684301d5173bb32200584b1fec1c9189
corazon03 <82195740+corazon03@users.noreply.github.com>
2022-06-11 22:16:34 -0600

    Registro sin footer

--- login.php
+++ login.php
@@ -4,29 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Biblioteca</title>
-    <link href="estilo.css" rel="stylesheet" type="text/css">
+    <title>Leyendo.com</title>
+    <link href="estilos.css" rel="stylesheet" type="text/css">
+    <link rel="icon" type="image/x-icon" href="img/Libro.ico">
 </head>
 <body>
     <div id="logo">
-        <h1>Biblioteca</h1>
+        <h1>Leyendo.com</h1>
     </div>
     <div id="inicioSesion">
         <div id="img">
             <img src="img/Libro.png">
         </div>
-        <div id="form">
-            <form action="index.php" method="post">
-                <label>Usuario</label><br/>
-                <input type="text" placeholder="Usuario..."><br/>
-                <label>Contraseña</label><br/>
-                <input type="text" placeholder="Contraseña..."><br/>
-                <input type="submit" value="Iniciar sesión">
+        <div id="formInicio">
+            <div>
+                <form action="index.php" method="post">
+                    <label>Usuario</label><br/>
+                    <input type="text" placeholder="Usuario..."><br/>
+                    <label>Contraseña</label><br/>
+                    <input type="password" placeholder="Contraseña..."><br/>
+                    <input type="submit" value="Iniciar sesión">
+                </form>
                 <div id="registro">
                     <p>¿Aún no tienes cuenta?</p>
-                    <a href="registro.php"><button>Regístrate</button></a>
+                    <a href="register.php"><button>Regístrate</button></a>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
 </body>
--- register.php
+++ register.php
@@ -4,9 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Leyendo.com</title>
+    <link href="estilos.css" rel="stylesheet" type="text/css">
+    <link rel="icon" type="image/x-icon" href="img/Libro.ico">
 </head>
 <body>
-    
+    <div id="logo">
+        <h1>Leyendo.com</h1>
+    </div>
+    <div id="register">
+        <div id="formRegis">
+            <form action="index.php" method="post">
+                <label>Nombre</label><br/>
+                <input type="text" placeholder="Nombre..."><br/>
+                <label>Apellido</label><br/>
+                <input type="text" placeholder="Apellido..."><br/>
+                <label>Correo</label><br/>
+                <input type="email" placeholder="Correo..."><br/>
+                <label>Usuario</label><br/>
+                <input type="text" placeholder="Usuario..."><br/>
+                <label>Contraseña</label><br/>
+                <input type="text" placeholder="Contraseña..."><br/>
+                <label>Confirmar contraseña</label><br/>
+                <input type="text" placeholder="Contraseña..."><br/>
+                <input type="submit" value="Registrarse">
+            </form>
+        </div>
+    </div>
 </body>
 </html>
