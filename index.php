617f215e43c72d4b0b40ddc95790d88a356aa291
corazon03 <82195740+corazon03@users.noreply.github.com>
2022-06-14 01:53:47 -0600

    index sin terminar

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leyendo.com</title>
+    <link href="estilo.css" rel="stylesheet" type="text/css">
+    <link rel="icon" type="image/x-icon" href="img/Libro.ico">
+</head>
+<body>
+    <div id="logoIndex">
+        <h1>Leyendo.com</h1>
+    </div>
+    <div id="find">
+        <img src="img/lupa.png">
+        <input type="text" placeholder="¿Qué buscas">
+        <img id="cerrar" src="img/cerrar.png">
+    </div>
+    <div id="inicioSesion">
+        
+    </div>
+</body>
+</html>
