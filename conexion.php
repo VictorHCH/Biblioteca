5dcf02bc9c171843a020724fd9c41b70ceac3d4a
VictorHCH <82013957+VictorHCH@users.noreply.github.com>
2022-06-15 00:59:46 -0600

    Mexicanada

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+    //Se realiza la conexion a la BD
+    $conexion = new mysqli('localhost','root','','libreria');
+
+    // if(isset($conexion)){
+    //     echo 'La BD esta conectada';
+    // }
+?>  
+
